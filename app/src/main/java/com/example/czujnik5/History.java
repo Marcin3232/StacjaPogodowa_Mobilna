431c0feae9f5f151b893d860b81b485fdc516a0f
Marcin3232 <58747227+Marcin3232@users.noreply.github.com>
2020-09-02 20:50:30 +0200

    updata

--- app/src/main/java/com/example/czujnik5/History.java
+++ app/src/main/java/com/example/czujnik5/History.java
@@ -1,10 +1,14 @@
 package com.example.czujnik5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,13 +19,19 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-
+InitzializeComponent();
     }
 
 
 
 
-
+public void InitzializeComponent(){
+        TabLayout tabLayout=findViewById(R.id.tabLayout);
+    TabItem tabTemp=findViewById(R.id.tab_temperatura);
+    TabItem tabCis=findViewById(R.id.tab_cisnienie);
+    TabItem tabWilg=findViewById(R.id.tab_wilgotnosc);
+    ViewPager viewPager=findViewById(R.id.viewPager);
+}
 
     //conect to database
     class connectToDataBase extends AsyncTask<Void, Void, Void>{
